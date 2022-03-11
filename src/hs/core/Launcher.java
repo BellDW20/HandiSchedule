1882a33bfac746fc9a5727f38dd0d98e74998301
amycunninghamm <cunninghamae19@gcc.edu>
2022-03-10 19:25:56 -0500

    Testing pushing/javafx button functionality

--- src/hs/core/Launcher.java
+++ src/hs/core/Launcher.java
@@ -2,6 +2,11 @@ package hs.core;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+
 
 public class Launcher extends Application {
 
@@ -12,6 +17,12 @@ public class Launcher extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("Handi Schedule");
+		//Testing adding a button
+		Button btn = new Button();
+		btn.setText("Welcome to HandiSchedule!");
+		StackPane sp = new StackPane();
+		sp.getChildren().add(btn);
+		stage.setScene(new Scene(sp, 800, 600));
 		stage.show();
 	}
 	
