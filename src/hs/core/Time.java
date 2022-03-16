5486c88f71c087c580a95dc49652311a4b2483cf
AndrewBeichner <BEICHNERAE19@gcc.edu>
2022-03-15 20:13:19 -0400

    Merge branch 'main' of git@github.com:BellDW20/HandiSchedule.git into main

---
+++ src/hs/core/Time.java
@@ -0,0 +1,55 @@
+package hs.core;
+
+public class Time {
+	
+	static final int AM = 0;
+	static final int PM = 1;
+	
+	int hour;
+	int minute;
+	int amOrPm;
+	
+	public Time(int hour, int minute, int amOrPm) {
+		hour = this.hour;
+		minute = this.minute;
+		amOrPm = this.amOrPm;
+	}
+	
+	
+	public boolean isAfter(Time time) {
+		if(time.hour < this.hour) {
+			return true;
+		}
+		else if(time.hour == this.hour) {
+			if(time.minute < this.minute) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean isBefore(Time time) {
+		if(time.hour > this.hour) {
+			return true;
+		}
+		else if(time.hour == this.hour) {
+			if(time.minute > this.minute) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	
+	
+
+}
