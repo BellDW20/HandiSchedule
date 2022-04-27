be4f4001854cccfcffb623fa4aa88ecf2a7c51f8
L23-GCC <ConradLJ19@gcc.edu>
2022-04-27 12:04:47 -0400

    Calendar Fix

--- src/hs/core/Schedule.java
+++ src/hs/core/Schedule.java
@@ -321,7 +321,6 @@ public class Schedule implements Serializable {
 				if (temp.getMeetingTimes().get(j).getDaysOfWeekString().contains("R")) {
 					g.drawRect(bufferWidth + headerWidth * 3, classY, classWidth, classHeight);
 					g.drawString(text, bufferWidth + (headerWidth * 3) + coursePadding, stringY + (classHeight / 2));
-
 				}
 				
 				if (temp.getMeetingTimes().get(j).getDaysOfWeekString().contains("F")) {
